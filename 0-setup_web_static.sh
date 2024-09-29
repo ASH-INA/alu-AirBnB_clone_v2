8bd5b3f8b6cd591fe692f12ae92cfcd0df5eab44
ash-ina <johngachungagitau@gmail.com>
2024-09-29 18:32:25 +0300

    Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,43 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
 
-apt-get update
-apt-get install -y nginx
+if ! dpkg -l | grep -q nginx; then
+    sudo apt update
+    sudo apt install -y nginx
+fi
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create a fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+# Create a symbolic link
+if [ -L /data/web_static/current ]; then
+    sudo rm /data/web_static/current
+fi
+sudo ln -s /data/web_static/releases/test /data/web_static/current
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
+# Give ownership to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
 
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
+# Update Nginx configuration
+if ! grep -q "hbnb_static" /etc/nginx/sites-available/default; then
+    echo "    location /hbnb_static/ {" | sudo tee -a /etc/nginx/sites-available/default > /dev/null
+    echo "        alias /data/web_static/current/;" | sudo tee -a /etc/nginx/sites-available/default > /dev/null
+    echo "        index index.html;" | sudo tee -a /etc/nginx/sites-available/default > /dev/null
+    echo "    }" | sudo tee -a /etc/nginx/sites-available/default > /dev/null
+fi
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+# Restart Nginx
+sudo service nginx restart
 
-service nginx restart
-
+# Exit successfully
 exit 0
